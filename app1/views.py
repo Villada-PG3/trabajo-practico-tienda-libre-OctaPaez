5ccebd0314989da86bdb107716804c42b821f307
Octavio Paez Scruzzi <o.paez.scruzzi@itsv.edu.ar>
2026-08-31 03:55:44 +0000

    feat: Incorporar seis productos ficticios al home

--- app1/views.py
+++ app1/views.py
@@ -6,7 +6,22 @@ def hello_world(request):
 # Create your views here.
 
 def home(request):
-    return render(request, "app1/home.html")
+    #return render(request, "app1/home.html")
+    productos = [ 
+    { "nombre": "Remera", "descripcion": "Remera de algodón suave y cómoda, ideal para usar todos los días. Disponible en diferentes talles y colores.", "stock": 15, "precio": 5000, },
+    { "nombre": "Pantalón", "descripcion": "Pantalón de jean clásico confeccionado con materiales resistentes y cómodos. Ideal para combinar con cualquier tipo de calzado.", "stock": 8, "precio": 12000, }, 
+    { "nombre": "Zapatillas", "descripcion": "Zapatillas deportivas livianas y cómodas, pensadas para actividades cotidianas y momentos de ejercicio.", "stock": 12, "precio": 25000, }, 
+    { "nombre": "Campera", "descripcion": "Campera impermeable y abrigada, perfecta para protegerse del frío y la lluvia durante los días de invierno.", "stock": 5, "precio": 30000, },
+    { "nombre": "Gorra", "descripcion": "Gorra deportiva ajustable con diseño moderno. Cuenta con una visera que ayuda a proteger del sol y es fácil de combinar con distintos estilos.", "stock": 20, "precio": 7000, }, 
+    { "nombre": "Mochila", "descripcion": "Mochila amplia y resistente con varios compartimentos para organizar libros, útiles, dispositivos y objetos personales.", "stock": 10, "precio": 18000, }, ]
+
+    return render(
+    request,
+    "app1/home.html",
+    {"productos": productos}
+    )
+
+
 
 def acerca_de_mi(request):
     return render(request, 'app1/acerca-de-mi.html')
